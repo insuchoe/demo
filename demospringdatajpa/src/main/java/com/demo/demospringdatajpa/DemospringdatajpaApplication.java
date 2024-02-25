750ad5858c6c6b1255a97c2cf8a4ea1ee8dacda7
insuchoe <cis940320@gmail.com>
2024-02-25 15:13:57 +0900

    demo spring data jpa start

---
+++ demospringdatajpa/src/main/java/com/demo/demospringdatajpa/DemospringdatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.demo.demospringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringdatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringdatajpaApplication.class, args);
+	}
+
+}
